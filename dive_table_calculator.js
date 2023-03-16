3a4fc2b938b3268ca0eafe3c5448b7b1b58f37b3
jacknsb <74638841+jacknsb@users.noreply.github.com>
2023-03-16 10:14:18 -0400

    Delete dive_table_calculator.js

--- dive_table_calculator.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('dive-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const depth = parseInt(document.getElementById('depth').value, 10);
-    const time = parseInt(document.getElementById('time').value, 10);
-    const result = calculateScubaDiveTable(depth, time);
-    
-    if (result) {
-        document.getElementById('pressure-group').textContent = 'Pressure Group: ' + result
